c4256f57fc71f3ca9001fabe383fc2a371057d04
Dmitry Mamonov <indexdm@yandex.ru>
2020-11-08 21:45:29 +0300

    add logging

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,18 +1,36 @@
 'use strict';
 
 const express = require(`express`);
-const chalk = require(`chalk`);
 const {ExitCode, API_PREFIX, StatusCode} = require(`../../constants`);
 const getMockData = require(`../lib/get-mock-data`);
 const routes = require(`../api`);
+const {getLogger} = require(`../lib/logger`);
 
 const DEFAULT_PORT = 3000;
 
 const app = express();
+const logger = getLogger({name: `api`});
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
+
 app.use(API_PREFIX, routes);
-app.use((req, res) => res.status(StatusCode.NOT_FOUND).send(`Not found`));
+
+app.use((req, res) => {
+  res.status(StatusCode.NOT_FOUND).send(`Not found`);
+  logger.error(`Route not found: ${req.url}`);
+});
+
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occured on processing request: ${err.message}`);
+});
 
 module.exports = {
   name: `--server`,
@@ -24,12 +42,12 @@ module.exports = {
       await getMockData();
       app.listen(port, (err) => {
         if (err) {
-          return console.error(chalk.red(`Ошибка при создании сервера: ${err}`));
+          return logger.error(`Ошибка при создании сервера: ${err}`);
         }
-        return console.info(chalk.green(`Сервер запущен на ${port}`));
+        return logger.info(`Сервер запущен на ${port}`);
       });
     } catch (err) {
-      console.error(`Произошла ошибка: ${err.message}`);
+      logger.error(`Произошла ошибка: ${err.message}`);
       process.exit(ExitCode.error);
     }
   }
