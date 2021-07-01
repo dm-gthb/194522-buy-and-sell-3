ae1ca4f67b401cdf9dd7be546a048b44244aa66e
dm-gthb <indexdm@yandex.ru>
2021-07-02 00:40:00 +0300

    store sessions in db

--- src/express/index.js
+++ src/express/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const express = require(`express`);
-const expressSession = require(`express-session`);
+const sequelize = require(`../service/lib/sequelize`);
 const path = require(`path`);
 const mainRoutes = require(`./routes/main`);
 const myRoutes = require(`./routes/my`);
 const offersRoutes = require(`./routes/offers`);
+const session = require(`./middlewares/session`);
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
 const UPLOAD_DIR = `upload`;
@@ -18,17 +19,13 @@ app.set(`views`, path.resolve(__dirname, TEMPLATES_DIR));
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
-
 app.use(express.urlencoded({extended: false}));
-
-app.use(expressSession({
-  secret: `SECRET`,
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(session(sequelize));
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
+
+sequelize.sync({force: false});
 
 app.listen(DEFAULT_PORT);
---
+++ src/express/middlewares/session.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const expressSession = require(`express-session`);
+const SequelizeStore = require(`connect-session-sequelize`)(expressSession.Store);
+
+module.exports = (sequelize) => {
+  const {SESSION_SECRET} = process.env;
+
+  if (!SESSION_SECRET) {
+    throw new Error(`SESSION_SECRET environment variable is not defined`);
+  }
+
+  const MILISECONDS_IN_MINUTE = 60000;
+
+  return expressSession({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    proxy: true,
+    store: new SequelizeStore({
+      db: sequelize,
+      expiration: MILISECONDS_IN_MINUTE * 3,
+      checkExpirationInterval: MILISECONDS_IN_MINUTE * 1,
+    })
+  });
+};
