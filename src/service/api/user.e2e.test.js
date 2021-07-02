33a7df761426f2e91381e1e4ccf456393356505c
dm-gthb <indexdm@yandex.ru>
2021-07-02 17:57:28 +0300

    update tests

--- src/service/api/user.e2e.test.js
+++ src/service/api/user.e2e.test.js
@@ -7,18 +7,19 @@ const initDb = require(`../lib/init-db`);
 const user = require(`./user`);
 const {UserService} = require(`../data-service`);
 const {StatusCode} = require(`../../constants`);
+const passwordUtils = require(`../lib/password`);
 
 const mockUsers = [
   {
     name: `Ivan Ivanov`,
     email: `ivan@ivan.com`,
-    passwordHash: `123456`,
+    passwordHash: passwordUtils.hashSync(`123456`),
     avatar: `img.jpg`,
   },
   {
     name: `Petr Petrov`,
     email: `petr@petr.com`,
-    passwordHash: `123456`,
+    passwordHash: passwordUtils.hashSync(`123456`),
     avatar: `img.jpg`,
   },
 ];
@@ -31,18 +32,22 @@ const mockCategories = [
 
 const mockOffers = [
   {
+    "userId": 1,
     "categories": [
       `Игры`,
       `Журналы`
     ],
     "comments": [
       {
+        "userId": 1,
         "text": `С чем связана продажа? Почему так дешёво? Неплохо, но дорого. А где блок питания?`
       },
       {
+        "userId": 1,
         "text": `А где блок питания?`
       },
       {
+        "userId": 1,
         "text": `Оплата наличными или перевод на карту? Неплохо, но дорого. Почему в таком ужасном состоянии?`
       }
     ],
@@ -153,5 +158,62 @@ describe(`API refuses to create new user if data is invalid`, () => {
       .post(`/user`)
       .send(busyEmailData)
       .expect(StatusCode.BAD_REQUEST);
+  });
+});
+
+describe(`API authenticate user if login data is valid`, () => {
+  let app;
+  let response;
+
+  const validCredentials = {
+    email: `ivan@ivan.com`,
+    password: `123456`,
+  };
+
+  beforeAll(async () => {
+    app = await createAPI();
+    response = await request(app)
+      .post(`/user/login`)
+      .send(validCredentials);
+  });
+
+  test(`Returns status code 200`, () => {
+    expect(response.statusCode).toBe(StatusCode.OK);
+  });
+
+  test(`User name is Ivan Ivanov`, () => {
+    expect(response.body.name).toBe(`Ivan Ivanov`);
+  });
+});
+
+describe(`API refuses to authenticate user if login data is not valid`, () => {
+  let app;
+
+  beforeAll(async () => {
+    app = await createAPI();
+  });
+
+  test(`Returns status code 401 UNAUTHORIZED`, async () => {
+    const invalidData = {
+      email: `asdfasdf@asdfasdf.com`,
+      password: `asdf`
+    };
+
+    await request(app)
+      .post(`/user/login`)
+      .send(invalidData)
+      .expect(StatusCode.UNAUTHORIZED);
+  });
+
+  test(`Returns status code 401 UNAUTHORIZED If password is wrong`, async () => {
+    const wrongPasswordData = {
+      email: `ivan@ivan.com`,
+      password: `not 123456`
+    };
+
+    await request(app)
+      .post(`/user/login`)
+      .send(wrongPasswordData)
+      .expect(StatusCode.UNAUTHORIZED);
   });
 });
