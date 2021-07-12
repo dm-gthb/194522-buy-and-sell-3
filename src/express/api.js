da05b196ce728daac130cd2792fae88dddcd4235
dm-gthb <indexdm@yandex.ru>
2021-07-12 19:27:52 +0300

    add delete personal articles route, fix template

--- src/express/api.js
+++ src/express/api.js
@@ -43,6 +43,12 @@ class API {
     });
   }
 
+  deleteOffer(id) {
+    return this._load(`/offers/${id}`, {
+      method: HttpMethod.DELETE
+    });
+  }
+
   search(query) {
     return this._load(`/search`, {params: {query}});
   }
--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -20,4 +20,10 @@ myRouter.get(`/comments`, async (req, res) => {
   res.render(`comments`, {user, offers: offers.slice(0, 3)});
 });
 
+myRouter.post(`/:offerId`, privateRoute, async (req, res) => {
+  const {offerId} = req.params;
+  await api.deleteOffer(offerId);
+  res.redirect(`/my`);
+});
+
 module.exports = myRouter;
